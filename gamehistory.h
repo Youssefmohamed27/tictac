1603414a0ee39187b6a8f39b7ada261f91294bd7
Youssefmohamed27 <162810360+Youssefmohamed27@users.noreply.github.com>
2024-06-27 10:10:13 +0300

    Update gamehistory.h

--- gamehistory.h
+++ gamehistory.h
@@ -1,24 +1,38 @@
 #ifndef GAMEHISTORY_H
 #define GAMEHISTORY_H
 
-#include <QWidget>
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QTableView>
+#include <QSqlTableModel>
+#include <QHeaderView> // Include QHeaderView for table header
 
-class QPushButton;
-class QVBoxLayout;
+QT_BEGIN_NAMESPACE
+namespace Ui { class gamehistory; }
+QT_END_NAMESPACE
 
-class gamehistory : public QWidget
+class gamehistory : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit gamehistory(QWidget *parent = nullptr);
+    explicit gamehistory(const QString &loggedInUsername, QWidget *parent = nullptr);
+    ~gamehistory();
 
 private slots:
     void goBackToSecondScreen();
 
 private:
+    Ui::gamehistory *ui;
+    QString loggedInUsername;
     QVBoxLayout *mainLayout;
     QPushButton *backButton;
+    QTableView *tableView;
+    QSqlTableModel *model;
+    QSqlDatabase db;
+
+    void updateModel();
 };
 
 #endif // GAMEHISTORY_H
